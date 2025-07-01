a08f6786bf4315a77483410da6815108fa7ce539
RyukooMaru <ryukoomaru@gmail.com>
2025-07-01 12:38:45 +0700

    Update controllerartiestories.php

--- app/Http/Controllers/App/artiestories/controllerartiestories.php
+++ app/Http/Controllers/App/artiestories/controllerartiestories.php
@@ -7,8 +7,11 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Artiestories;
 use App\Models\ArtiestoriesIMG;
-use Illuminate\Support\Facades\Storage;
 use getID3;
+use Google_Client;
+use Google_Service_Drive;
+use Google_Service_Drive_DriveFile;
+use Google_Service_Drive_Permission;
 
 class controllerartiestories extends Controller
 {
@@ -53,30 +56,82 @@ class controllerartiestories extends Controller
                 return back()->withErrors(['alert' => 'Durasi video tidak boleh lebih dari 60 detik!']);
             }
         }
-        //$storagePath = session('username') . '/artiestories/' . $randomString;
-        //foreach ($files as $index => $file) {
-        //    $extension = $file->getClientOriginalExtension();
-        //    $filename = session('username') . '_' . date('Ymd_His') . '_' . $index . '.' . $extension;
-        //    Storage::disk('public')->putFileAs($storagePath, $file, $filename);
-        //    $filepath = 'storage/' . $storagePath . '/' . $filename;
-        //    ArtiestoriesIMG::create([
-        //        'artiestoriesid' => $post->artiestoriesid,
-        //        'konten' => $filepath,
-        //    ]);
-        //}
-        // Anda tetap bisa menggunakan variabel ini untuk mendefinisikan struktur folder
-        $storagePath = session('username') . '/artiestories/' . $randomString;
         foreach ($files as $index => $file) {
             $extension = $file->getClientOriginalExtension();
             $filename = session('username') . '_' . date('Ymd_His') . '_' . $index . '.' . $extension;
-            $path = $file->storeAs($storagePath, $filename, 'azure');
-            $url = Storage::disk('azure')->url($path);
+            $pathgoogle = session('username') . "/artiestories/" . $randomString;
+            $googleDriveUrl = $this->uploadToGoogleDrive($file, $filename, $pathgoogle);
             ArtiestoriesIMG::create([
                 'artiestoriesid' => $post->artiestoriesid,
-                'konten' => $url,
+                'konten' => $googleDriveUrl,
             ]);
         }
         return redirect()->route('artieses')->with(['alert' => 'Artiestories mu sudah di publish!']);
     }
+    private function createDriveFolder($service, $folderName, $parentId)
+    {
+        $fileMetadata = new \Google_Service_Drive_DriveFile([
+            'name' => $folderName,
+            'mimeType' => 'application/vnd.google-apps.folder',
+            'parents' => [$parentId]
+        ]);
 
+        $folder = $service->files->create($fileMetadata, [
+            'fields' => 'id'
+        ]);
+
+        return $folder->id;
+    }
+    private function findOrCreateFolder($service, $folderName, $parentId)
+    {
+        $query = sprintf(
+            "mimeType='application/vnd.google-apps.folder' and name='%s' and '%s' in parents and trashed=false",
+            addslashes($folderName),
+            $parentId
+        );
+        $response = $service->files->listFiles([
+            'q' => $query,
+            'spaces' => 'drive',
+            'fields' => 'files(id, name)',
+            'pageSize' => 1,
+        ]);
+        if (count($response->files) > 0) {
+            return $response->files[0]->id;
+        }
+        return $this->createDriveFolder($service, $folderName, $parentId);
+    }
+
+    private function uploadToGoogleDrive($file, $filename, $subFolderPath = null)
+    {
+        $credentialsPath = storage_path('app/google/credentials.json');
+        $mainFolderId = '1A2B3C4D5E6F7G8H9';
+        $client = new \Google_Client();
+        $client->setAuthConfig($credentialsPath);
+        $client->addScope(\Google_Service_Drive::DRIVE);
+        $service = new \Google_Service_Drive($client);
+        $parentId = $mainFolderId;
+        if ($subFolderPath) {
+            $parts = explode('/', $subFolderPath);
+            foreach ($parts as $part) {
+                $parentId = $this->findOrCreateFolder($service, $part, $parentId);
+            }
+        }
+        $fileMetadata = new \Google_Service_Drive_DriveFile([
+            'name' => $filename,
+            'parents' => [$parentId],
+        ]);
+        $content = file_get_contents($file->getPathname());
+        $uploadedFile = $service->files->create($fileMetadata, [
+            'data' => $content,
+            'mimeType' => $file->getMimeType(),
+            'uploadType' => 'multipart',
+            'fields' => 'id',
+        ]);
+        $permission = new \Google_Service_Drive_Permission([
+            'type' => 'anyone',
+            'role' => 'reader',
+        ]);
+        $service->permissions->create($uploadedFile->id, $permission);
+        return 'https://drive.google.com/file/d/' . $uploadedFile->id . '/view';
+    }
 }
