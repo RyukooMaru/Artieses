df9fe40ec22a961bfdfca541dd570f99930cc11d
RyukooMaru <ryukoomaru@gmail.com>
2025-07-01 15:41:51 +0700

    Update controllerartievides.php

--- app/Http/Controllers/App/artievides/controllerartievides.php
+++ app/Http/Controllers/App/artievides/controllerartievides.php
@@ -13,28 +13,33 @@ use Google_Service_Drive_Permission;
 
 class controllerartievides extends Controller
 {
-    private function uploadToGoogleDriveDirect($file, $finalFileName)
+    private function uploadToGoogleDriveDirect($file)
     {
         $credentialsPath = storage_path('app/google/credentials.json');
         $mainFolderId = '1dAtghVH4G3rgOoypIkdqUKAh6uslcHIQ';
         $client = new \Google_Client();
         $client->setAuthConfig($credentialsPath);
         $client->addScope(\Google_Service_Drive::DRIVE);
-        
         $service = new \Google_Service_Drive($client);
-
+        $tempName = 'temp_' . time() . '.' . $file->getClientOriginalExtension();
         $fileMetadata = new \Google_Service_Drive_DriveFile([
-            'name' => $finalFileName,
+            'name' => $tempName,
             'parents' => [$mainFolderId],
         ]);
         $content = file_get_contents($file->getPathname());
         $mimeType = $file->getMimeType();
+
         $uploadedFile = $service->files->create($fileMetadata, [
             'data' => $content,
             'mimeType' => $mimeType,
             'uploadType' => 'multipart',
             'fields' => 'id',
         ]);
+        $newName = $uploadedFile->id . '.' . $file->getClientOriginalExtension();
+        $updateMetadata = new \Google_Service_Drive_DriveFile([
+            'name' => $newName
+        ]);
+        $service->files->update($uploadedFile->id, $updateMetadata);
         $permission = new \Google_Service_Drive_Permission([
             'type' => 'anyone',
             'role' => 'reader',
@@ -42,6 +47,7 @@ class controllerartievides extends Controller
         $service->permissions->create($uploadedFile->id, $permission);
         return $uploadedFile->id;
     }
+
 
     public function uploadFile(Request $request)
     {
@@ -69,12 +75,8 @@ class controllerartievides extends Controller
         $randomString = generateUniqueCodevides();
         $video = $request->file('video');
         $thumbnail = $request->file('thumbnail');
-        $videoFilename = session('username') . '_video_' . time() . '.' . $video->getClientOriginalExtension();
-        $thumbnailFilename = session('username') . '_thumbnail_' . time() . '.' . $thumbnail->getClientOriginalExtension();
-        $videoPath = "Video/" . session('username');
-        $thumbPath = "Thumbnail/" . session('username');
-        $videoFileId = $this->uploadToGoogleDriveDirect($videoPath, $videoFilename);
-        $thumbFileId = $this->uploadToGoogleDriveDirect($thumbPath, $thumbnailFilename);
+        $videoFileId = $this->uploadToGoogleDriveDirect($video);
+        $thumbFileId = $this->uploadToGoogleDriveDirect($thumbnail);
         Artievides::create([
             'userid' => session('userid'),
             'codevides' => $randomString,
