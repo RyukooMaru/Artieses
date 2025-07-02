175b6f158a923f8c88505cc67eb43c67dd9f75b0
Bagas Indra Lesmana <107906655+bagasil123@users.noreply.github.com>
2025-07-02 20:00:11 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo "Starting custom nginx configuration..."
-# Salin file konfigurasi kustom kita ke lokasi konfigurasi Nginx yang aktif
-cp /home/site/wwwroot/nginx/default /etc/nginx/sites-enabled/default
+# Salin file konfigurasi PHP kustom ke direktori yang benar
+cp /home/site/wwwroot/custom.ini /usr/local/etc/php/conf.d/
 
-# Muat ulang Nginx untuk menerapkan konfigurasi baru
-service nginx reload
-echo "Nginx reloaded with custom configuration."
+# Jalankan skrip startup bawaan Azure setelahnya
+/opt/startup/startup.sh
